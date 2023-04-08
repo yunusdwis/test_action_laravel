28ed0feccf96fa137a82f4f53f9250d349917a41
yunusdwis <yunus.dwis@gmail.com>
2023-04-09 03:09:48 +0700

    wohhoheh

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    echo '<center><h1>HELLO WORLD!<BR>from Laravel<br>yang empat nihh</h1><center>';
+    return view('test');
 });
